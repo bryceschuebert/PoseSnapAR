76a53345dd82b1cc6c5513d292052381d2ce73c2
bryceschuebert <bryceschuebert@gmail.com>
2023-03-18 22:11:39 -0400

    ..

--- frontend/src/components/ProtectedRoute/ProtectedRoute.js
+++ frontend/src/components/ProtectedRoute/ProtectedRoute.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Navigate } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const ProtectedRoute = ({ element, ...rest }) => {
